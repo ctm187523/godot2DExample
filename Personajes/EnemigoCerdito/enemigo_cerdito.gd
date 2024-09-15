06b065be9b41abc882477d585bb4b2a7327e730c
CTM <clemente1875gmail.com>
2024-09-15 22:15:40 +0200

    Inteligencia al cerdito, detecta al jugador cambia de estado y persigue al jugador

--- Personajes/EnemigoCerdito/enemigo_cerdito.gd
+++ Personajes/EnemigoCerdito/enemigo_cerdito.gd
@@ -1,3 +1,82 @@
 extends Personajes       #heredamos de la clase Personajes
 
 
+var direccion = -1  #empieza caminado a la izquierda
+
+#inicializamos Nodos
+@onready var raysuelo : RayCast2D = $RayCasts/RayCastSuelo
+@onready var rayMuro : RayCast2D = $RayCasts/RayCastSMuro
+@onready var rayos = $RayCasts  #contiene los 2 raycast anteriores
+
+@onready var rayCastPlayer : RayCast2D = $RayCastPlayerDetector
+@onready var anim  = $AnimationPlayer
+
+var player
+
+var canChangeDirection = true   #flag para manejaar el cambio de direccion pasado un pequeño tiempo y no sea instantaneo
+
+#usamos un enum para definir los estados del cerdito(máquina de estados)
+enum estados {ANGRY,PATRULLAR}
+var estadoActual = estados.PATRULLAR
+
+#funciones
+#funcion que se ejecuta la incio
+func _ready():
+	anim.play("walk")   #ponemos la animacion en walk
+	speed = 60   #speed lo hereda de Personajes, le damos el valor deseado
+
+#funcion para manejar las Físicas
+func _physics_process(delta):
+	velocity.x = direccion * speed    #calculamos la velocidad en x, dada por la direccion y speed
+	
+	#si no esta en el suelo le aplicamos gravedad
+	if !is_on_floor():
+		velocity.y += 9
+	move_and_slide()   #funcion de Godot para aplicar el movimiento
+
+#funcion que se ejecuta a cada frame
+func _process(delta):
+	
+	#si el cerdito colisiona con el personaje
+	#al colisionar la variable player tendra el valor de la colision, que es el player, de esta manera la condicion
+	#solo se cumple si no hemos detectado al player con los raycast
+	if player == null and rayCastPlayer.is_colliding():
+		var colision = rayCastPlayer.get_collider() #obtenemos con get_collider con que es lo que ha colisionado el cerdito
+		if colision.is_in_group("Player"):  #comprobamos que colisione con el Player
+			player = colision   #a la variable player inicializada arriba le damos el valor de la colision,el player y en la condicion de esta no sera nulo el player
+			estadoActual = estados.ANGRY  #cambiamos el estado 
+			
+			
+	#si hemos colisionado con el player obtenemos en el metodo de arriba el player y
+	#el cerdito toma la direccion del player, comprobamos que el estado sea Angry los raycast han detectado al player
+	#lo de player != null se podria quitar ya que ya comprobamos el estado con el Angry
+	if estadoActual == estados.ANGRY and  player != null:
+		anim.play("runAngry")   #cambiamos el sprite al sprite del cerdito enfadado
+		var directionPlayer = global_position.direction_to((player.global_position))
+		if directionPlayer.x <0:    #si es menor que cero va el cerdito a la izquierda, cambiamos la direccion
+			direccion=-1
+		elif  directionPlayer.x > 0:
+			direccion = 1			#si es mayor que cero va el cerdito a la derecha, cambiamos la direccion
+	
+
+	#comprobamos que este en estado de Patrullar a detectado al personaje con sus raycast
+	if estadoActual == estados.PATRULLAR:
+		#si  colisiona con el muro el raycast que apunta el muro lo multiplicamos
+		#por -1 para cambiar la direccion
+		#cambiamos tambien del grupo del Raycasts rayos inicializado arriba la scala
+		#la multiplicamos por -1 y de esta manera la flecha donde apunta el raycast
+		#cambia de sentido en la x, ver en el inspector de RayCast y cambiar la escala 
+		# a 1 y -1 para ver como cambia el sentido de las flechas del Raycast
+		if canChangeDirection and  (rayMuro.is_colliding() or !raysuelo.is_colliding()):
+			canChangeDirection = false  #si cambia de direccion ponemos la variable canChangeDirection en false
+			$RayCasts/RayTimer.start()  #inciamos el Timer, al acabar el Timer llama a la funcion conectada de abajo _on_ray_timer_timeout 
+			direccion *= -1
+			rayos.scale.x *= -1
+	$Sprite2D.flip_h = true if direccion == 1 else false #cambiamos la direccion del sprite si direccion es 1
+
+
+#metodo de la conexion de la señal del Timer, para que al colisionar pase un tiempo
+#para que no cambie del direccion al momento
+func _on_ray_timer_timeout():
+	canChangeDirection = true  #volvemos a poner en true la variable para que si colisiona cambie de direccion ya pasado un tiempo
+	pass # Replace with function body.
