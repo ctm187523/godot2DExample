6c2099de4f7b6000052364ad3422d55f5cfcf773
CTM <clemente1875gmail.com>
2024-10-13 08:04:41 +0200

    añadimos sierras con movimientos al juego

--- Personajes/MovingPlatform/plataforma.gd
+++ Personajes/MovingPlatform/plataforma.gd
@@ -3,22 +3,32 @@ extends Path2D
 
 
 @export var platformSpeed: float = .2   #ponemos @export para poder manipular la variable en el editor
+var isRight = true   #variable para controlar las velociadades
+
 
 func _process(delta):
 	
-	#comandos del tool añadido arriba
-	if Engine.is_editor_hint(): #corre con el editor
-		pass
-	if not Engine.is_editor_hint():	#solo corre en el juego, si ponemos el codigo de abajo en este if en el editor no se moveria la plataforma solo se moveria en el juego
-		pass
+	#comandos del tool añadido arriba, los dejamos vacios
+	#if Engine.is_editor_hint(): #corre con el editor
+		#pass
+	#if not Engine.is_editor_hint():	#solo corre en el juego, si ponemos el codigo de abajo en este if en el editor no se moveria la plataforma solo se moveria en el juego
+		#pass
 		
 	#igualamos las posiciones	
 	$Plataforma.global_position = $PathFollow2D.global_position   #hacemos que la posicion global del Plataforma donde contiene el Sprite sea igual a la del PathFollow2d
 	
-	#progress_ratio es una propiedad de PathFollow2D ver en el inspector que es
-	#la progresion del camino, si es menor que 1 lo aumentamos
-	if $PathFollow2D.progress_ratio < 1:
+	#si va hacia la derecha se va aumentando, controlado por la varialbe isRight
+	if isRight:
 		$PathFollow2D.progress_ratio += platformSpeed * delta
+	#a la izquierda disminuye
 	else:
-		$PathFollow2D.progress_ratio = 0   #si no es menor que 1 lo reiniciamos a 0 para que vuelva a empezar el camino
+		$PathFollow2D.progress_ratio -= platformSpeed * delta
+	
+	#progress_ratio es una propiedad de PathFollow2D ver en el inspector que es
+	#la progresion del camino
+	if $PathFollow2D.progress_ratio >= .95:
+		isRight = false
+	elif $PathFollow2D.progress_ratio <= .05:
+		isRight = true
+		
 
---
+++ Trampas/sierra.gd
@@ -0,0 +1,39 @@
+@tool     #comportamiento de la sierra en el editor
+extends Node2D
+var dmg = 1 
+
+@onready var sprite =  $SierraVerdadera/Sprite #inicializamos el Sprite de la sierra
+@onready var path = $Path2D/PathFollow2D  #incializamos e path
+@onready var sierra = $SierraVerdadera
+
+@export var platformSpeed: float = .2   #ponemos @export para poder manipular la variable en el editor
+var isRight = true   #variable para controlar las velociadades
+
+
+func _process(delta):
+	sprite.rotate(deg_to_rad(450*delta))  #hacemos que la sierra rote, usamos deg_to_rad para pasar de grados a radianes, lo multiplicamos por delta para que corra igual en qualquier dispositivo
+	
+	#igualamos las posiciones	
+	sierra.global_position = path.global_position   #hacemos que la posicion global del Plataforma donde contiene el Sprite sea igual a la del PathFollow2d
+	
+	#si va hacia la derecha se va aumentando, controlado por la varialbe isRight
+	if isRight:
+		path.progress_ratio += platformSpeed * delta
+	#hacia la izquierda disminuye 
+	else:
+		path.progress_ratio -= platformSpeed * delta
+	
+	#progress_ratio es una propiedad de PathFollow2D ver en el inspector que es
+	#la progresion del camino
+	if path.progress_ratio >= .95:
+		isRight = false
+	elif path.progress_ratio <= .05:
+		isRight = true
+
+#señal conectada del nodo DamagePlayer, al interactuar con la
+#sierra quitamos vida al Player, llamando a su metodo takeDamage
+func _on_damage_player_body_entered(body):
+	if body is Player:
+		body.takeDamage(dmg)
+
+
